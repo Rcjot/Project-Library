a1f839b50a8952dc7461d925563d9dd1d63eadf3
Rcjot <rcappy495@gmail.com>
2024-06-17 23:18:08 +0800

    general logic

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const library = document.querySelector('#library');
+const bookname = document.querySelector('#bookname');
+const bookauthor = document.querySelector('#bookauthor')
+const bookpages = document.querySelector('#bookpages')
+const bookread = document.querySelector('#bookread')
+const submit = document.querySelector('#submit')
 
 const myLibrary = [];
+
+submit.addEventListener("click", () => {
+    let name = bookname.value;
+    let author = bookauthor.value;
+    let pages = bookpages.value;
+    let read = bookread.checked;
+    addBooktoLibrary(name, author, pages, read);
+    //addBooktoLibrary(bookname.value, bookauthor.value, bookpages.value, bookread.checked);
+
+})
+
 
 
 function Book(name, author, pages, read, index) {
@@ -27,17 +43,11 @@ function Book(name, author, pages, read, index) {
             console.log(myLibrary.length)
         });
     }
-
-
-    
-    // add function here to delete the book when button pressed
-
 }
 
 function addBooktoLibrary(name, author, pages, read, index) {
     const newBook = new Book(name, author, pages, read, index);
     myLibrary.push(newBook);
-
     
     for (let book of myLibrary) {
         book.createBook();
@@ -47,19 +57,25 @@ function addBooktoLibrary(name, author, pages, read, index) {
 
 
 
+
 const mybook1 = new Book('myBook', 'anon', 234, true, myLibrary.length);
-
 myLibrary.push(mybook1);
-
 addBooktoLibrary('adfadf', 'asdfa', 23, false, myLibrary.length);
-
-
 console.log(myLibrary);
-
-
 console.log('asfd' + myLibrary[0]);
 
+// modal creation
+const modalbutton = document.querySelector('#clickme');
+const dialog = document.querySelector("dialog");
+const closebutton = document.querySelector("dialog #closedlg");
 
+modalbutton.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+closebutton.addEventListener("click", () => {
+    dialog.close();
+})
 
 
 /*
