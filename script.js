680d36c8a4568c7bb69ffd644bd9be123f7d9e2f
Rcjot <rcappy495@gmail.com>
2024-06-22 23:59:55 +0800

    convert to class

--- script.js
+++ script.js
@@ -25,56 +25,113 @@ submit.addEventListener("click", (event) => {
     bookread.checked = false;
 })
 
+class Book {
+    constructor (name, author, pages, read, index) {
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.index = index;
+    }
+    createDom() {
+        this.bookPlaced = document.createElement("div");
+        this.bookPlaced.classList.add('bookPlaced');
 
-
-function Book(name, author, pages, read, index) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.index = index;
-
-    this.createBook = function(){
-
-        const bookPlaced = document.createElement("div");
-        bookPlaced.classList.add('bookPlaced');
-        currentlibrarycontainer.appendChild(bookPlaced); //append book to Library
-        const deleteButton = document.createElement("button");
-        deleteButton.classList.add('deleteButton'); // .deleteButton
-        
-        deleteButton.textContent = 'delete Book'
-        deleteButton.addEventListener("click", () => {
+        this.deleteButton = document.createElement("button");
+        this.deleteButton.classList.add('deleteButton'); // .deleteButton
+        this.deleteButton.textContent = 'delete Book'
+        this.deleteButton.addEventListener("click", () => {
             delete myLibrary[this.index];
             bookPlaced.remove();
             console.log('it works!' + this.index);
             console.log(myLibrary);
             console.log(myLibrary.length)
         });
-        const readCheckBox = document.createElement('input');
-        const readLabel = document.createElement('label');
-        readLabel.htmlFor = 'readCheckBox';
-        readLabel.appendChild(document.createTextNode('finished'));
-        readCheckBox.setAttribute('type', 'checkbox');
-        readCheckBox.classList.add('bookread1');
-        readCheckBox.checked = this.read;
-        const bookPlacedName = document.createElement('h1');
-        const bookPlacedAuthor = document.createElement('p');
-        const bookPlacedPages = document.createElement('p');
-        const checklabeldiv = document.createElement('div');
-        checklabeldiv.classList.add('checklabeldiv');
-        bookPlacedName.textContent = this.name;
-        bookPlacedAuthor.textContent = 'by ' + this.author;
-        bookPlacedPages.textContent = this.pages + ' pages';
-        bookPlaced.appendChild(bookPlacedName)
-        bookPlaced.appendChild(bookPlacedAuthor)
-        bookPlaced.appendChild(bookPlacedPages)
-        checklabeldiv.appendChild(readLabel);
-        checklabeldiv.appendChild(readCheckBox);
-        bookPlaced.appendChild(checklabeldiv);
-        bookPlaced.appendChild(deleteButton);//append button to book
-        
+
+        this.readCheckBox = document.createElement('input');
+        this.readLabel = document.createElement('label');
+        this.readLabel.htmlFor = 'readCheckBox';
+        this.readLabel.appendChild(document.createTextNode('finished'));
+        this.readCheckBox.setAttribute('type', 'checkbox');
+        this.readCheckBox.classList.add('bookread1');
+        this.readCheckBox.checked = this.read;
+
+        this.bookPlacedName = document.createElement('h1');
+        this.bookPlacedAuthor = document.createElement('p');
+        this.bookPlacedPages = document.createElement('p');
+        this.checklabeldiv = document.createElement('div');
+        this.checklabeldiv.classList.add('checklabeldiv');
+    }
+    appendDom(){
+        currentlibrarycontainer.appendChild(this.bookPlaced); //append book to Library
+        this.bookPlacedName.textContent = this.name;
+        this.bookPlacedAuthor.textContent = 'by ' + this.author;
+        this.bookPlacedPages.textContent = this.pages + ' pages';
+        this.bookPlaced.appendChild(this.bookPlacedName)
+        this.bookPlaced.appendChild(this.bookPlacedAuthor)
+        this.bookPlaced.appendChild(this.bookPlacedPages)
+        this.checklabeldiv.appendChild(this.readLabel);
+        this.checklabeldiv.appendChild(this.readCheckBox);
+        this.bookPlaced.appendChild(this.checklabeldiv);
+        this.bookPlaced.appendChild(this.deleteButton);//append button to book
+    }
+    createBook() {
+        this.createDom();
+        this.appendDom();
     }
 }
+
+
+
+
+// function Book(name, author, pages, read, index) {
+//     this.name = name;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.index = index;
+
+//     this.createBook = function(){
+
+//         const bookPlaced = document.createElement("div");
+//         bookPlaced.classList.add('bookPlaced');
+//         currentlibrarycontainer.appendChild(bookPlaced); //append book to Library
+//         const deleteButton = document.createElement("button");
+//         deleteButton.classList.add('deleteButton'); // .deleteButton
+        
+//         deleteButton.textContent = 'delete Book'
+//         deleteButton.addEventListener("click", () => {
+//             delete myLibrary[this.index];
+//             bookPlaced.remove();
+//             console.log('it works!' + this.index);
+//             console.log(myLibrary);
+//             console.log(myLibrary.length)
+//         });
+//         const readCheckBox = document.createElement('input');
+//         const readLabel = document.createElement('label');
+//         readLabel.htmlFor = 'readCheckBox';
+//         readLabel.appendChild(document.createTextNode('finished'));
+//         readCheckBox.setAttribute('type', 'checkbox');
+//         readCheckBox.classList.add('bookread1');
+//         readCheckBox.checked = this.read;
+//         const bookPlacedName = document.createElement('h1');
+//         const bookPlacedAuthor = document.createElement('p');
+//         const bookPlacedPages = document.createElement('p');
+//         const checklabeldiv = document.createElement('div');
+//         checklabeldiv.classList.add('checklabeldiv');
+//         bookPlacedName.textContent = this.name;
+//         bookPlacedAuthor.textContent = 'by ' + this.author;
+//         bookPlacedPages.textContent = this.pages + ' pages';
+//         bookPlaced.appendChild(bookPlacedName)
+//         bookPlaced.appendChild(bookPlacedAuthor)
+//         bookPlaced.appendChild(bookPlacedPages)
+//         checklabeldiv.appendChild(readLabel);
+//         checklabeldiv.appendChild(readCheckBox);
+//         bookPlaced.appendChild(checklabeldiv);
+//         bookPlaced.appendChild(deleteButton);//append button to book
+        
+//     }
+// }
 
 function addBooktoLibrary(name, author, pages, read, index) {
     const newBook = new Book(name, author, pages, read, index);
